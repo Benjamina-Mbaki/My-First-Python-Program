80b6ff776493a32b5040eb43546bc6d4d953daa3
Benjamina Mbaki <benjaminambaki@gmail.com>
2026-07-29 13:59:25 +0200

    Add files via upload

---
+++ # My First Python Program.py
@@ -0,0 +1,14 @@
+# My First Python Program
+
+print("=================================")
+print("     MY FIRST PYTHON PROGRAM")
+print("=================================")
+
+name = "Benjamina"
+age = 20  # Change this to your age
+
+print("Hello! My name is", name)
+print("I am", age, "years old.")
+print("My favourite colour is Lavender.")
+print("I enjoy learning Python, Java, C++, SQL, and building websites.")
+print("Thank you for reading my first Python program! 🐍")
